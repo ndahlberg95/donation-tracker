ef85d5bbd1cef6468b42cab764399d45074eb264
Nora Dahlberg <ndahlberg95@gmail.com>
2021-03-08 20:17:51 -0700

    for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,4 +23,21 @@ function displayData(){
     .text(data.items[0].name)
 }
 
+var min;
+var max;
+var sum = 0;
+
+for ( var i = 0; i < arr.length()-1; i++){
+    if (i==0){
+        min = arr[i]
+        max = arr[i]
+    } else {
+        if (arr[i] < min)
+            min = arr[i];
+        if (arr[i] > max)
+            max = arr[i];
+    }
+    sum = sum + arr[i];
+} 
+
 displayData()
